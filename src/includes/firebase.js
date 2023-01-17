777182604c37ed98bd0d418f62de553b2e9b7ee8
thalesleite <thalesaleite@gmail.com>
2023-01-17 11:09:02 +0000

    uploading files

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmC2oydsiE-NVFZPlwhVkQ5QxyrHdlXbw",
@@ -14,7 +15,9 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
 
-export { auth, db, usersCollection };
+export { auth, db, usersCollection, songsCollection, storage };
