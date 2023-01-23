ce29efa8db7f1afd0e0ff18231803c4d6294dab3
thalesleite <thalesaleite@gmail.com>
2023-01-23 13:30:24 +0000

    test pinia actions, mock promises and test router

--- src/components/__tests__/songItem.spec.js
+++ src/components/__tests__/songItem.spec.js
@@ -22,4 +22,23 @@ describe("SongItem.vue", () => {
 
     expect(compositionAuthor.text()).toBe(song.display_name);
   });
+
+  test("renders song.docID in id attribute", () => {
+    const song = {
+      docID: "abc",
+    };
+
+    const wrapper = shallowMount(SongItem, {
+      propsData: {
+        song,
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+
+    expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
+  });
 });
